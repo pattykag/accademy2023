1f1f44589e5bbf1eb8807ff589bad680d6cf32cb
Patricia Kristal Amesty Guanipa <13051483>
2023-09-06 20:59:34 +0000

    Update Recordings

--- ejercicio5-Disquera/srv/model-view.cds
+++ ejercicio5-Disquera/srv/model-view.cds
@@ -5,18 +5,32 @@ service labelservice {
     entity Bands                 as projection on my.Bands;
     entity Bands_Musicians       as projection on my.Bands_Musicians;
     entity Disks                 as projection on my.Disks;
-    entity Distributions         as projection on my.Distributions;
     entity Disks_Distributions   as projection on my.Disks_Distributions;
+
+    @cds.redirection.target
+    entity Distributions         as projection on my.Distributions;
 
     @cds.redirection.target
     entity Recordings            as projection on my.Recordings;
 
+    // 1
+    entity BandDistribution      as
+        select from my.Distributions {
+            name                as distributedBy,
+            disk.disk.band.name as bandName,
+            disk.disk.name      as diskName,
+            disk.disk.tracks
+        };
+
+    // 2
     entity MusicianRecordingDate as
         select from my.Recordings {
             *,
             musician.name     as musicianName,
             musician.lastname as musicianLastname,
-            disk.name         as diskName
+            disk.name         as diskName,
+            disk.band.name    as bandName,
+            disk.band.genre
         }
         excluding {
             createdAt,
--- ejercicio5-Disquera/srv/model-view.js
+++ ejercicio5-Disquera/srv/model-view.js
@@ -1,7 +1,7 @@
 const cds = require('@sap/cds');
 
 module.exports = cds.service.impl(async (srv) => {
-    const { Musicians } = srv.entities;
+    const { Musicians, Recordings } = srv.entities;
 
     srv.before('CREATE', 'Recordings', async (req) => {
         const { hourQuantity } = req.data;
@@ -12,6 +12,28 @@ module.exports = cds.service.impl(async (srv) => {
             if (hourQuantity >= 6) {
                 req.data.promo = true;
                 req.data.hourQuantity = hourQuantity + 2;
+            }
+        } catch (error) {
+            req.reject(400, error);
+        }
+    });
+
+    srv.before('UPDATE', 'Recordings', async (req) => {
+        const { ID, hourQuantity } = req.data;
+        console.log('hourQuantity', hourQuantity);
+
+        try {
+            const query = await SELECT.from(Recordings, { ID });
+            console.log('query', query);
+
+            if (query) {
+                if (hourQuantity >= 6 && query.promo === false) {
+                    req.data.promo = true;
+                    req.data.hourQuantity = hourQuantity + 2;
+                } else if (hourQuantity < 6 && query.promo === true) {
+                    req.data.promo = false;
+                    req.data.hourQuantity = hourQuantity - 2;
+                }
             }
         } catch (error) {
             req.reject(400, error);
@@ -65,5 +87,14 @@ module.exports = cds.service.impl(async (srv) => {
         } catch (error) {
             req.reject(400, error);
         }
+    });
+
+    // Muestras
+    srv.before('*', Musicians, async (req) => {
+        console.log(`se realizó el método ${req.method}.`);
+    });
+
+    srv.before(['CREATE', 'UPDATE'], 'Distributions', async () => {
+        console.log('Prueba de create y update en la entidad Distributions');
     });
 });
